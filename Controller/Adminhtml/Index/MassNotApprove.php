8fd44fa14d2a5c38f30906553daf939226727927
phuongtt <phuongtt@mageplaza.com>
2019-01-18 08:23:46 +0700

    add massaction approve and not approve on customer grid

---
+++ Controller/Adminhtml/Index/MassNotApprove.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_CustomerApproval
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+namespace Mageplaza\CustomerApproval\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Ui\Component\MassAction\Filter;
+use Magento\Customer\Model\ResourceModel\Customer\CollectionFactory;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Eav\Model\Entity\Collection\AbstractCollection;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Customer\Controller\Adminhtml\Index\AbstractMassAction;
+use Mageplaza\CustomerApproval\Helper\Data;
+
+/**
+ * Class MassNotApprove
+ * @package Mageplaza\CustomerApproval\Controller\Adminhtml\Index
+ */
+class MassNotApprove extends AbstractMassAction
+{
+    /**
+     * @var CustomerRepositoryInterface
+     */
+    protected $customerRepository;
+
+    /**
+     * @var Data
+     */
+    protected $helperData;
+
+    /**
+     * MassNotApprove constructor.
+     *
+     * @param Context                     $context
+     * @param Filter                      $filter
+     * @param CollectionFactory           $collectionFactory
+     * @param CustomerRepositoryInterface $customerRepository
+     * @param Data                        $helperData
+     */
+    public function __construct(
+        Context $context,
+        Filter $filter,
+        CollectionFactory $collectionFactory,
+        CustomerRepositoryInterface $customerRepository,
+        Data $helperData
+    )
+    {
+        parent::__construct($context, $filter, $collectionFactory);
+        $this->customerRepository = $customerRepository;
+        $this->helperData         = $helperData;
+    }
+
+    /**
+     * @param AbstractCollection $collection
+     *
+     * @return \Magento\Backend\Model\View\Result\Redirect|\Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface
+     * @throws \Exception
+     */
+    protected function massAction(AbstractCollection $collection)
+    {
+        $customersUpdated = 0;
+        foreach ($collection->getAllIds() as $customerId) {
+            #not approve customer account
+            $this->helperData->notApprovalCustomerById($customerId);
+            $customersUpdated++;
+        }
+
+        if ($customersUpdated) {
+            $this->messageManager->addSuccess(__('A total of %1 record(s) were updated.', $customersUpdated));
+        }
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        $resultRedirect->setPath($this->getComponentRefererUrl());
+
+        return $resultRedirect;
+    }
+}
